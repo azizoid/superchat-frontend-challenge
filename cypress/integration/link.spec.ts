8219a0fa0a7e4ec65281729208b28385f7be5371
azizoid <shaqhuseynov@gmail.com>
2022-01-04 12:45:52 +0100

    test: use variables in tests

--- cypress/integration/link.spec.ts
+++ cypress/integration/link.spec.ts
@@ -1,25 +1,31 @@
+
+
 describe('Home Page', () => {
+  const id = '472c0af8'
+  const username = 'azizoid'
+  const repo = 'nam.az'
+
   beforeEach(() => {
-    cy.intercept('GET', '/api/githublink/**', (req) => {
+    cy.intercept('GET', `/api/githublink/${id}`, (req) => {
       req.reply({ fixture: 'getLinkData.json' })
     }).as('getLinkData')
-    cy.intercept('GET', 'https://api.github.com/users/**', (req) => {
+    cy.intercept('GET', `https://api.github.com/users/${username}`, (req) => {
       req.reply({ fixture: 'getUser.json' })
     }).as('getUser')
-    cy.intercept('GET', 'https://api.github.com/repos/**', (req) => {
+    cy.intercept('GET', `https://api.github.com/repos/${username}/${repo}`, (req) => {
       req.reply({ fixture: 'getRepoDetails.json' })
     }).as('getRepoDetails')
-    cy.intercept('GET', 'https://api.github.com/repos/**/contributors?per_page=10&anon=true', (req) => {
+    cy.intercept('GET', `https://api.github.com/repos/${username}/${repo}/contributors?per_page=10&anon=true`, (req) => {
       req.reply({ fixture: 'getContributors.json' })
     }).as('getContributors')
   })
 
   it('should find main elements of sharable page', () => {
-    cy.visit('http://localhost:3000/r/472c0af8-d1de-4de2-a8c4')
+    cy.visit(`http://localhost:3000/r/${id}`)
 
     cy.get('[data-cy=basic-info]').contains('Basic Info')
-    cy.get('[data-cy=preview-username]').should('contain', 'azizoid')
-    cy.get('[data-cy=preview-repo]').should('contain', 'nam.az')
+    cy.get('[data-cy=preview-username]').should('contain', username)
+    cy.get('[data-cy=preview-repo]').should('contain', repo)
     cy.get('button[data-cy=preview-theme]').should('have.class', 'btn-dark').get('[data-cy=preview-stars]').contains('43')
     cy.get('[data-cy=preview-contributors').get('[data-cy=contributor-item]').should('have.length', 2)
   })
