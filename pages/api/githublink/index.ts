f376417eae0b3ce8f44dcb89c814a4005fc5fac5
azizoid <shaqhuseynov@gmail.com>
2022-01-04 08:16:28 +0100

    api: add post and get requests

---
+++ pages/api/githublink/index.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { v4 as uuidv4 } from "uuid"
+
+import { githubLinksData, Data, GithubLinksDataProps } from "../../../store/_data"
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | GithubLinksDataProps>
+) {
+  switch (req.method) {
+    case 'POST':
+      const data = { ...JSON.parse(req.body), id: uuidv4() }
+      githubLinksData.push(data)
+      //return the saved data in case if we need to reuse it (which we do)
+      res.status(200).json({ data })
+      break
+    case 'GET':
+      res.status(200).json({ data: githubLinksData })
+    default:
+      res.setHeader('Allow', ['PUT'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
