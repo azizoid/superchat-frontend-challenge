f24a4378714e248ecc6f0e25663678375cd02345
azizoid <shaqhuseynov@gmail.com>
2022-01-04 12:46:42 +0100

    test: check if repository list is removed when user does not exist

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,15 +1,18 @@
 describe('Home Page', () => {
+  const username = 'azizoid'
+  const repo = 'nam.az'
+
   beforeEach(() => {
-    cy.intercept('GET', 'https://api.github.com/users/**', (req) => {
+    cy.intercept('GET', `https://api.github.com/users/${username}`, (req) => {
       req.reply({ fixture: 'getUser.json' })
     }).as('getUser')
-    cy.intercept('GET', 'https://api.github.com/users/**/repos', (req) => {
+    cy.intercept('GET', `https://api.github.com/users/${username}/repos`, (req) => {
       req.reply({ fixture: 'getRepos.json' })
     }).as('getRepos')
-    cy.intercept('GET', 'https://api.github.com/repos/**', (req) => {
+    cy.intercept('GET', `https://api.github.com/repos/${username}/${repo}`, (req) => {
       req.reply({ fixture: 'getRepoDetails.json' })
     }).as('getRepoDetails')
-    cy.intercept('GET', 'https://api.github.com/repos/**/contributors?per_page=10&anon=true', (req) => {
+    cy.intercept('GET', `https://api.github.com/repos/${username}/${repo}/contributors?per_page=10&anon=true`, (req) => {
       req.reply({ fixture: 'getContributors.json' })
     }).as('getContributors')
 
@@ -36,12 +39,29 @@ describe('Home Page', () => {
   })
 
   it('gets repository list by username', () => {
-    cy.get('#repositoryList').should("not.exist")
+    cy.get('[data-cy=repository-list]').should("not.exist")
 
-    cy.get('input[type=text]').type('azizoid')
+    cy.get('input[type=text]').type(username)
     cy.get('button[type=submit]')
       .click()
       .get('[data-cy=repository-list]').should('exist')
+  })
+
+  it('removes old repository list if user does not exist', () => {
+
+    cy.get('input[type=text]').type(username)
+    cy.get('button[type=submit]')
+      .click()
+      .get('[data-cy=repository-list]').should('exist')
+
+    cy.intercept('GET', 'https://api.github.com/users/fakeUser', (req) => {
+      req.reply({ fixture: 'getFakeUser.json' })
+    }).as('getFakeUser')
+
+    cy.get('input[type=text]').type('fakeUser')
+    cy.get('button[type=submit]')
+      .click()
+    cy.get('[data-cy=repository-list]').should("not.exist")
   })
 
   it('changes theme on theme button click', () => {
@@ -50,7 +70,7 @@ describe('Home Page', () => {
 
   it('tests the whole cycle', () => {
     cy.get('[data-cy=noRepositoryPanel]')
-    cy.get('input[type=text]').type('azizoid')
+    cy.get('input[type=text]').type(username)
 
     cy.get('button[type=submit]')
       .click()
