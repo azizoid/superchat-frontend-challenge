1f135dcad249e92ed376359a184b514d7be9d20b
azizoid <shaqhuseynov@gmail.com>
2022-01-04 08:52:11 +0100

    style: set style for sharable page

--- pages/r/[id].tsx
+++ pages/r/[id].tsx
@@ -4,17 +4,21 @@ import { NextPage } from "next"
 import { PageStateProps } from "../index"
 import { Loader } from "../../ui/Loader/Loader"
 
+import styles from "../../styles/r.page.module.scss"
+
 export const GetGithubLink: NextPage = () => {
   const [pageState, setPageState] = useState(PageStateProps.Loading)
 
   return (
     <div className="container">
-      <div className="col-md-12 col-lg-8 offset-lg-2">
-        {pageState === PageStateProps.Loading && (
-          <span className="list-group-item text-center">
-            <Loader />
-          </span>
-        )}
+      <div className={styles.previewPage}>
+        <div className="col-md-12 col-lg-8 offset-lg-2">
+          {pageState === PageStateProps.Loading && (
+            <span className="list-group-item text-center">
+              <Loader />
+            </span>
+          )}
+        </div>
       </div>
     </div>
   )
