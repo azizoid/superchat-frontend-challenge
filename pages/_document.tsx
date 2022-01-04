667a7ebefb40087403dd75cd36e186524fcf1d53
azizoid <shaqhuseynov@gmail.com>
2022-01-04 10:37:51 +0100

    style: add navbar to the top

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="author" content="Aziz" />
+        </Head>
+        <body className="d-flex flex-column h-100">
+          <nav className="navbar navbar-light bg-light">
+            <div className="container">
+              <a className="navbar-brand" href="#">
+                SuperChat link generator
+              </a>
+            </div>
+          </nav>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
