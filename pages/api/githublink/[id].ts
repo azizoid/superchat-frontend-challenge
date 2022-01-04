aebd70cf6ddc44ade4f5ac40fe9fcf32821ff282
azizoid <shaqhuseynov@gmail.com>
2022-01-04 09:15:15 +0100

    api: add sharable router api

---
+++ pages/api/githublink/[id].ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { githubLinksData, Data } from "../../../store/_data"
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id } = req.query
+
+
+  if (req.method === 'GET') {
+    const result = githubLinksData.find(link => link.id === id)
+
+    if (result) {
+      res.status(200).json({ ...result })
+    } else {
+      res.status(404).end(`link ${id} Not Found`)
+    }
+  }
+}
